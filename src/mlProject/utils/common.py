49392e2de44d52bae3be2e8e1cfdec5a16d2b0f4
Md Riad <riad5089@gmail.com>
2024-02-04 11:07:23 +0600

    yaml file updated

--- src/mlProject/utils/common.py
+++ src/mlProject/utils/common.py
@@ -1,11 +1,12 @@
+
 import os
-from box.exceptions import BoxValueError 
+from box.exceptions import BoxValueError
 import yaml
 from mlProject import logger
 import json
 import joblib
-from ensure import ensure_annotations # for ensure package
-from box import ConfigBox # for type checking
+from ensure import ensure_annotations
+from box import ConfigBox
 from pathlib import Path
 from typing import Any
 
@@ -124,6 +125,3 @@ def get_size(path: Path) -> str:
     """
     size_in_kb = round(os.path.getsize(path)/1024)
     return f"~ {size_in_kb} KB"
-
-
-
